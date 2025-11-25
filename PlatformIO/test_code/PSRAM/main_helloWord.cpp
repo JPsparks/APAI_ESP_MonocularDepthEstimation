d098c80d643be2ffe49ab024f452a840e84c299d
JPsparks <jacopo.pesaresi.02@gmail.com>
2025-11-25 10:25:17 +0100

    new file :starting camera phase

---
+++ PlatformIO/test_code/PSRAM/main_helloWord.cpp
@@ -0,0 +1,22 @@
+// #include <Arduino.h>
+
+// void setup() {
+//   Serial.begin(115200);
+
+//   delay(5000);
+//   bool a = psramInit();
+//   // delay(5000);
+  
+//   if (a) {
+//       Serial.println("PSRAM OK!");
+//   } else {
+//       Serial.println("PSRAM ERROR!");
+//   }
+  
+//   Serial.println("Modello pronto!");
+// }
+
+// void loop() {
+//   Serial.println("Sto bene!");
+//   delay(500);
+// }
